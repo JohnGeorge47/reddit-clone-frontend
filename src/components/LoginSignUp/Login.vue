024683891f2672383c1dfae3cd6627ae5daf47c9
John <john.g@roofandfloor.com>
2018-06-07 20:28:15 +0530

    added the login signup routes

---
+++ src/components/LoginSignUp/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Now if you are a first time user do sign in here</h2>
+    <router-link :to="{ name: 'signup'}"><h3>Sign up here!</h3></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      msg: "Lets get You Logged in"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+/* eslint-disable */
+
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "@/components/LoginSignUp/Login";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: "/",
+      name: "Login",
+      component: Login
     },
-  ],
+    {
+      path: "/signup",
+      name: "signup",
+      component: Login
+    }
+  ]
 });

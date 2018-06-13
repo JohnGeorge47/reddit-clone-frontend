c055f95e04a885e1590859b0b4d4b11792e2b8d6
John <john.g@roofandfloor.com>
2018-06-13 16:45:53 +0530

    added the forms

--- src/components/LoginSignUp/Login.vue
+++ src/components/LoginSignUp/Login.vue
@@ -1,18 +1,71 @@
+
 <template>
   <div class="hello">
+    <div v-if="this.$route.name=='Login'">
     <h1>{{ msg }}</h1>
-    <h2>Now if you are a first time user do sign in here</h2>
+    <h2>Now if you are a first time user do sign up here</h2>
+    <div class="loginSignupForm">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="exampleInputGroup2"
+                label-for="exampleInput2">
+    <b-form-input id="exampleInput2"
+                  type="text"
+                  v-model="form.name"
+                  required
+                  placeholder="Enter name">
+    </b-form-input>
+  </b-form-group>
+  <b-form-group id="exampleInputGroup3"
+            label-for="exampleInput3">
+<b-form-input id="exampleInput3"
+              type="password"
+              v-model="form.name"
+              required
+              placeholder="Enter your password">
+</b-form-input>
+</b-form-group>
+    </b-form>
+</div>
     <router-link :to="{ name: 'signup'}"><h3>Sign up here!</h3></router-link>
+  </div>
+  <div v-else>
+      <h1>{{ signupmsg }}</h1>
+      <h2>You can Login by clicking below</h2>
+      <router-link :to="{ name: 'Login'}"><h3>Log in!</h3></router-link>
+  </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+
 export default {
   name: "Login",
   data() {
     return {
-      msg: "Lets get You Logged in"
+      msg: "Lets get You Logged in",
+      signupmsg: "Sign up here",
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn"
+      ],
+      show: true
     };
+  },
+  computed: {
+    state() {
+      console.log("sxx");
+      console.log(this.name);
+    }
   }
 };
 </script>
@@ -33,5 +86,20 @@ li {
 }
 a {
   color: #42b983;
+}
+@media screen and (min-width: 768px) {
+  .loginSignupForm {
+    margin: auto;
+    width: 40%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .loginSignupForm {
+    margin: auto;
+    width: 80%;
+  }
+}
+#input2 {
+  margin-top: 15px;
 }
 </style>
